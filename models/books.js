9782d07fcab2bd563f5e6564cfd976484ca7cca7
Jonathon Stillson <jcstillson77@gmail.com>
2023-06-19 21:36:33 -0400

    added some things

---
+++ models/books.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const bookSchema = new Schema({
+    title: String,
+    author: String,
+    status: String,
+})
